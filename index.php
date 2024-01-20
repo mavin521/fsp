fe1ac33edc317a19436e8813927cb71c72731604
mavin521 <155206381+mavin521@users.noreply.github.com>
2024-01-20 12:12:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,14 @@
     <form method="post" action="generate_m3u.php">
         <label for="channels">电视频道（用英文模式下的逗号分隔）:</label>
         <input type="text" id="channels" name="channels">
-        <input type="submit" value="生成M3U文件">
+        
+        <label for="action">选择操作：</label>
+        <select id="action" name="action">
+            <option value="generate_m3u">生成M3U文件</option>
+            <option value="play_directly">直接播放</option>
+        </select>
+        
+        <input type="submit" value="执行操作">
     </form>
 </body>
 </html>
