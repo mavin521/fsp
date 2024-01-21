8bc04caef7005f5b86b7aba0e3595691b4a17ebd
mavin521 <155206381+mavin521@users.noreply.github.com>
2024-01-21 20:08:18 +0800

    Create famous_channels.php

---
+++ famous_channels.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'CCTV1', 'CCTV2', 'CCTV3', 'CCTV4', 'CCTV5', 'CCTV6', 'CCTV7', 'CCTV8', 'CCTV9', 'CCTV10',
+    '湖南卫视', '江苏卫视', '浙江卫视', '北京卫视', '东方卫视', '安徽卫视', '黑龙江卫视', '吉林卫视', '辽宁卫视', '山东卫视',
+    '香港电视台', '澳门电视台', '凤凰卫视', '星空卫视', '翡翠台', 'TVB经典', 'ViuTV', 'NOW直播', '濠視', 'J2',
+    '中天电视', '华视', '台视', '东森电影', '中视', '华视', '民视', '大愛', '寰宇新闻', '公视HD',
+    'BBC News', 'CNN', 'Fox News', 'ABC News', 'CNBC', 'Bloomberg', 'Al Jazeera English', 'DW News', 'France 24', 'Russia Today',
+    'Discovery Channel', 'National Geographic', 'History Channel', 'Animal Planet', 'Travel Channel', 'Food Network', 'HGTV', 'BBC Earth', 'MTV', 'VH1',
+    'ESPN', 'Fox Sports', 'NBA TV', 'NFL Network', 'Golf Channel', 'Tennis Channel', 'MLB Network', 'Sky Sports News', 'Eurosport', 'CBS Sports',
+    'CCTV5+', 'Guangdong Sports', 'LeSports', 'WWE Network', 'Star Sports', 'beIN Sports', 'Sky Sports F1', 'NFL RedZone', 'NHL Network', 'MUTV',
+    'Cartoon Network', 'Disney Channel', 'Nickelodeon', 'Boomerang', 'CBeebies', 'Nick Jr.', 'Disney Junior', 'PBS Kids', 'BabyTV', 'KidsCo',
+    'MTV Asia', 'Channel V', 'Zing', 'B4U Music', '9XM', 'MCM', 'VH1 India', 'Radio City', 'Mastiii', 'Sun Music',
+];
