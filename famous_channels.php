c2cb822fd9cc919662276c835c2f54106367ec1b
mavin521 <155206381+mavin521@users.noreply.github.com>
2024-01-24 18:41:29 +0800

    Update famous_channels.php

--- famous_channels.php
+++ famous_channels.php
@@ -3,13 +3,13 @@
 return [
     'CCTV1', 'CCTV2', 'CCTV3', 'CCTV4', 'CCTV5', 'CCTV6', 'CCTV7', 'CCTV8', 'CCTV9', 'CCTV10','CCTV13','CCTV5+',
     '湖南卫视', '江苏卫视', '浙江卫视', '北京卫视', '东方卫视', '安徽卫视', '黑龙江卫视', '吉林卫视', '辽宁卫视', '山东卫视',
-    '凤凰卫视', '星空卫视', '翡翠台', 'TVB经典', 'ViuTV', 'NOW', 
-    '中天电视', '华视', '台视', '东森电影', '中视', '华视', '民视', '大愛', '寰宇新闻', '公视HD',
+    '凤凰卫视', '星空卫视', '翡翠台', 'TVB经典', 'ViuTV', 'NOW', '澳门莲花', '凤凰资讯', 
+    '中天', '华视', '台视', '东森电影', '中视', '华视', '民视', '大愛', '寰宇新闻', '公视',
     'BBC News', 'CNN', 'Fox News', 'ABC News', 'CNBC', 'Bloomberg', 'Al Jazeera English', 'DW News', 'France 24', 'Russia Today',
     'Discovery Channel', 'National Geographic', 'History Channel', 'Animal Planet', 'Travel Channel', 'Food Network', 'HGTV', 'BBC Earth', 'MTV', 
     'ESPN', 'Fox Sports', 'NBA TV', 'NFL Network', 'Golf Channel', 'Tennis Channel', 'MLB Network', 'Sky Sports News', 'Eurosport', 'CBS Sports',
     'LeSports', 'WWE Network', 'Star Sports', 'beIN Sports', 'Sky Sports F1', 'NFL RedZone', 'NHL Network', 'MUTV',
     'Cartoon Network', 'Disney Channel', 'Nickelodeon', 'Boomerang', 'CBeebies', 'Disney Junior', 'PBS Kids', 'BabyTV', 'KidsCo',
-    'MTV Asia', 'Channel V', '9XM', 'MCM', 'VH1 India', '龙祥电影', '咪咕体育', '咪咕视频', 
+    'MTV Asia', 'Channel V', 'MCM', 'VH1 India', '龙祥电影', '咪咕体育', '咪咕视频', 
     'UFC', 'MAX', 'CHC高清电影', '爱历史', 'NEWTV', '漫威电影', 'CHC家庭影院', '爱历史', '求索记录','求索动物', 'HBO', 
 ];
